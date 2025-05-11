6d4448771fba76eb090eccd7f8e5ccec5d5b7b00
Laosdy <95322945+Laosdy@users.noreply.github.com>
2025-05-11 15:57:35 +0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,9 +8,13 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+dnf5 config-manager addrepo -y --from-repofile=https://repo.secureblue.dev/secureblue.repo
+
+
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 install -y akmod-wl trivalent
 
 # Use a COPR Example:
 #
