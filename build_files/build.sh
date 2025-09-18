650d7766d0f6bf1ac7ff16c54b315336196ea10c
TriVoxel <caden@trivoxel.io>
2025-09-17 22:47:37 -0600

    Update build.sh
    Fix build issue

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,16 +14,4 @@ dnf5 config-manager addrepo -y --from-repofile=https://repo.secureblue.dev/secur
 
 # this installs a package from fedora repos
 dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf5 install -y akmod-wl broadcom-wl
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
-systemctl enable wl
+dnf5 install -y akmod-wl broadcom-wl kmod-wl
