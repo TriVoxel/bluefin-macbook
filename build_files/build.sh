114884b778395be9147c4db568c05f650ba61066
TriVoxel <caden@trivoxel.io>
2025-09-18 11:19:58 -0600

    Update build.sh
    Please, be the one! Hazzah!

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,6 +14,5 @@ dnf5 config-manager addrepo -y --from-repofile=https://repo.secureblue.dev/secur
 
 # this installs a package from fedora repos
 dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-#dnf5 install -y akmod-wl broadcom-wl kmod-wl
-ls /tmp/akmods-rpms/kmods
+dnf5 versionlock remove kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 dnf5 install -y /tmp/akmods-rpms/kmods/*wl*.rpm /tmp/akmods-kernel-rpms/kernel*.rpm
