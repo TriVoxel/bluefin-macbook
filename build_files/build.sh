daa97513d4e3b9699434c62a089e70df3c3f48f0
TriVoxel <caden@trivoxel.io>
2025-09-18 15:28:03 -0600

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,13 +10,10 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 dnf5 config-manager addrepo -y --from-repofile=https://repo.secureblue.dev/secureblue.repo
 
-skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods:main-"$(rpm -E %fedora)" dir:/tmp/akmods
-AKMODS_TARGZ=$(jq -r '.layers[].digest' </tmp/akmods/manifest.json | cut -d : -f 2)
-tar -xvzf /tmp/akmods/"$AKMODS_TARGZ" -C /tmp/
+
 
 # this installs a package from fedora repos
 dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf5 versionlock delete kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 dnf5 remove -y kernel* # mega oof
-ls /tmp/akmods/
-dnf5 install -y /tmp/akmods/kmods/*wl*.rpm /tmp/akmods-kernel-rpms/kernel*.rpm
+dnf5 install -y /tmp/akmods-rpms/kmods/*wl*.rpm /tmp/akmods-kernel-rpms/kernel*.rpm
