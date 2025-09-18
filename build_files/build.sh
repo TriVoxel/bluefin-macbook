7dbdfada0359c9a8b473af6e3894e5cfdee8a9b0
TriVoxel <caden@trivoxel.io>
2025-09-17 22:11:40 -0600

    Adjust Base Image

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,7 +14,7 @@ dnf5 config-manager addrepo -y --from-repofile=https://repo.secureblue.dev/secur
 
 # this installs a package from fedora repos
 dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf5 install -y akmod-wl trivalent android-tools
+dnf5 install -y akmod-wl broadcom-wl
 
 # Use a COPR Example:
 #
@@ -26,3 +26,5 @@ dnf5 install -y akmod-wl trivalent android-tools
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable wl
+akmods
